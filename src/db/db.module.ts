e72ca4440342e7546472dd1cc9e7751513a6c8f6
Guilherme Garcia <guilherme.garciaortega@gmail.com>
2025-05-19 17:06:36 -0300

    feat: add database module and TypeORM configuration
    - Introduced DbModule to handle database connections using TypeORM.
    - Updated package.json to include TypeORM and MySQL dependencies.
    - Added migration scripts to package.json for easier database management.
    - Created typeOrm.migration-config.ts for migration configuration.
    - Removed unused IsDate import from task.dto.ts.
    - Added .nvmrc file to specify Node.js version.

---
+++ src/db/db.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DbModule {}
